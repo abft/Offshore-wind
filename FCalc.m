56bde0575809e22241a375796cc9f496e57e1d29
avidortelem <43061767+avidortelem@users.noreply.github.com>
2018-09-16 14:20:32 +0200

    Add files via upload

--- FCalc.m
+++ FCalc.m
@@ -1,40 +1,41 @@
 function [F_drag,F_inert,F_tot,M]=FCalc(f,h,g,D,a,iWheeler,num)
-% f = 0.0833;
-% h =30;
-% g = 9.81;
-% D = 6;
-% a = 3;
-% iWheeler = true;
 
 %iWheeler being true if wheeler stretching considered, false otherwise
-rho = 1024;
-omega = 2*pi*f;
+
+% Initialisation 
+C_D = 1; % drag coefficient used for the monopile
+C_M = 2; % inertia coefficient
+rho = 1024; % sea water density
+omega = 2*pi*f; 
 k =kSolve(f,g,h);
-C_D = 1;
-C_M = 2;
-A = pi*D^2/4;
+A = pi*D^2/4; % cross area of the monopile
+
+F_drag=zeros(1,200);
+F_inert=zeros(1,200);
+F_tot=zeros(1,200); % Inline force
+M=zeros(1,200); % overturning moment
 
 for t=1:200
-    eta = a*cos(omega*t);
+    eta = a*cos(omega*t); % free surface elevation at x=0 
     z_phys = linspace(-h,eta,num);
-    dz = (z_phys(2)-z_phys(1))*ones(1,40); %constant
     if iWheeler ==true
         z_calc = (z_phys - eta)/(1+eta/h);
     else
-        z_calc = z_phys;
+        z_calc = z_phys; % no stretching
     end
     for i=1:num
+        % the horizontal velocity and acceleration are calculated with
+        % z_calc
         u(i) = omega*a*cosh(k*(z_calc(i)+h))/sinh(k*h)*cos(omega*t); %airy
         dudt(i) = -omega^2*a*cosh(k*(z_calc(i)+h))/sinh(k*h)*sin(omega*t);
         dF_drag(i) = 1/2*rho*C_D*abs(u(i))*u(i);
         dF_inert(i) = rho*C_M*A*dudt(i);
-        dM(i) = (dF_drag(i) + dF_inert(i))*(h + z_calc(i));
+        dM(i) = (dF_drag(i) + dF_inert(i))*(h + z_phys(i));
     end
     F_drag(t) = trapz(z_phys,dF_drag);
     F_inert(t) = trapz(z_phys,dF_inert); 
     M(t) = trapz(z_phys,dM);
 end
 F_tot = F_drag + F_inert;
-
 
 end
--- Part1.m
+++ Part1.m
@@ -1,141 +1,168 @@
+% Report 01 : WaveLoads Monopile
+% Part 1 : Linear monochromatic waves
+
+%% Initiliasation of the workspace
 clear all
 close all
 clc
 
-h = 30; %depth m
-D = 6; %pile diameter m
-g = 9.81; %gravity m/s^2
+%% Initilisation of the global parameters.
+h = 30; % depth (m)
+D = 6; % monopile diameter (m)
+g = 9.81; % gravity (m/s^2)
+H = 6; % Regular wave height (m)
+T = 12; % Regular wave period (s)
+f = 1/T; % wave frequency (Hz)
 
-%% Part 1
-%% 2.
-H = 6; %height m
-T = 12; %period s
+%% Question 1
 
-omega = 2*pi/T;
-f = 1/T;
-k = kSolve(f,g,h);
-x=0;
-L = 2*pi/k;
+k = kSolve(f,g,h); % calcul of the wavenumber thanks to kSolve.m
+L = 2*pi/k; % Wavelength
 
-t = linspace(0,100,500);
+%% Question 2: Plot time series of eta, u at z=0 and dudt at z=0.
 
-eta = H/2*cos(omega.*t - k*x);
-u_z0 = omega.*H/2*cosh(k*h)/sinh(k*h)*cos(omega.*t); %horizontal velocity
-dudt_z0 = -omega.^2.*H/2*cosh(k*h)/sinh(k*h)*sin(omega.*t); %horizontal acc
+omega = 2*pi/T; % wave angular frequency 
+f = 1/T; % wave frequency
 
+x=0; % We look the time series at x=0 (would just be a different phase compare to other x)
+t = linspace(0,100,500); % initialisation of the time vector
 
+% Calculate eta, u_z0, dudt_z0 thanks to the linear theory (Aire waves)
+eta = H/2*cos(omega.*t-k*x); % surface elevation
+u_z0 = omega.*H/2*cosh(k*h)/sinh(k*h)*cos(omega.*t-k*x); % horizontal velocity
+dudt_z0 = -omega.^2.*H/2*cosh(k*h)/sinh(k*h)*sin(omega.*t-k*x); %horizontal acc
 
+% Compute Keulegan Carpenter nulber
 
-figure()
-hold on
-plot(t,eta)
-plot(t,u_z0)
-plot(t,dudt_z0)
-hold off
-xlabel('time')
-%ylabel('','Fontsize',14)
-legend({'$\eta$ (t)','$u(t)_{|z=0}$','$\frac{\partial u}{\partial t}_{|z=0}$'}, ... 
+Um=max(u_z0);
+Kc=Um*T/D;
+
+% Plot the time series
+fQ2=figure();
+plot(t,eta,t,u_z0,t,dudt_z0)
+xlabel('t(s)')
+legend({'$\eta$ [m]','$u(t)_{|z=0}$ [m/s]','$\frac{\partial u}{\partial t}_{|z=0} [m/s^2]$ '}, ... 
     'Interpreter','latex','Location','Southeast')
 title('Time series')
 enhance_plot('TIMES',16,1.5)
-
-%eta et u_z0 même phase, dudt_z0 déphasage
-%eta augmente, dp augmente, mouvement -> vitesse augmente
-%quand eta = 0 acceleration purement verticale et maximale donc u =0 quand eta
-% max, acceleration purement horizontale et decroissante donc u=max
-% schéma trajectoire particule
+saveas(fQ2,'Q2.png');
 
 
-%% 3.
-%D/L < 0.2 so we can use Morison equation
 
-a = H/2;
-t = [1:200];
-[F_drag_w,F_inert_w,F_tot_w] = FCalc(f,h,g,D,a,true,40);
+%% Question 3.
+% D/L < 0.2 so we can use Morison equation (slender body)
 
+a = H/2; % wave amplitude
+t_Fcalc=[1:1:200]; % time vector used in FCalc
 
-[F_drag,F_inert,F_tot] = FCalc(f,h,g,D,a,false,40);
+% Calcul the inline forces with Wheeler stretching
+[F_drag_w,F_inert_w,F_tot_w,M_w] = FCalc(f,h,g,D,a,true,40);
 
-dif = abs(F_tot - F_tot_w)/F_tot; %e^-4
-%explain
-% H<<h error due to exp not big enough
+% Calcul the inline forces without Wheeler stretching
+[F_drag,F_inert,F_tot,M] = FCalc(f,h,g,D,a,false,40);
 
-figure()
-plot(t,10^(-6)*F_drag,t,10^(-6)*F_inert)
-xlabel('time','Fontsize',14)
+% Quantify the Wheeler stretching effect 
+Wheeler_effect = abs(max(F_tot) - max(F_tot_w))/max(F_tot); 
+
+% Plot
+
+% Drag and inertial forces
+fQ3_1=figure();
+plot(t_Fcalc,10^(-6)*F_drag,t_Fcalc,10^(-6)*F_inert)
+xlabel('time (s)','Fontsize',14)
 ylabel('Inline force [MN]','Fontsize',14)
 legend({'Drag force','Inertial force'},'Fontsize',12)
 title('Drag and inertial forces','Fontsize',14)
 enhance_plot('TIMES',16,1.5)
+saveas(fQ3_1,'Q3_1.png');
 
-figure()
-plot(t,10^(-6)*F_tot,'g',t,10^(-6)*F_tot_w,'k--')
-xlabel('time','Fontsize',14)
+% Total inline force
+fQ3_2=figure();
+plot(t_Fcalc,10^(-6)*F_tot,'g',t_Fcalc,10^(-6)*F_tot_w,'k--')
+xlabel('time (s)','Fontsize',14)
 ylabel('Inline force [MN]','Fontsize',14)
 legend({'Without wheeler stretching','With wheeler stretching'},'Fontsize',12)
 title('Total inline force','Fontsize',14)
 enhance_plot('TIMES',16,1.5)
+saveas(fQ3_2,'Q3_2.png');
 
-figure()
-plot(t,10^(-3)*F_drag,'g',t,10^(-3)*F_drag_w,'k--')
-xlabel('time','Fontsize',14)
+% Drag force with and without wheeler stretching
+fQ3_3=figure();
+plot(t_Fcalc,10^(-3)*F_drag,'g',t_Fcalc,10^(-3)*F_drag_w,'k--')
+xlabel('time (s)','Fontsize',14)
 ylabel('Force [kN]','Fontsize',14)
 legend({'Without wheeler stretching','With wheeler stretching'},'Fontsize',12)
 title('Drag force','Fontsize',14)
 enhance_plot('TIMES',16,1.5)
+saveas(fQ3_3,'Q3_3.png');
 
-figure()
-plot(t,10^(-6)*F_inert,'g',t,10^(-6)*F_inert_w,'k--')
-xlabel('time','Fontsize',14)
+% Inertial force with and without wheeler stretching
+
+fQ3_4=figure();
+plot(t_Fcalc,10^(-6)*F_inert,'g',t_Fcalc,10^(-6)*F_inert_w,'k--')
+xlabel('time (s)','Fontsize',14)
 ylabel('Force [MN]','Fontsize',14)
 legend({'Without wheeler stretching','With wheeler stretching'},'Fontsize',12)
 title('Inertial force','Fontsize',14)
 enhance_plot('TIMES',16,1.5)
+saveas(fQ3_4,'Q3_4.png');
 
-%% 4.
+%% Question 4.
 %according to tempel et machin with D/L=0.04 CM ~2 
 
-%% 5 
-eta = a*cos(omega.*t);
+%% Question 5 
 
-[~,~,F_tot,M] = FCalc(f,h,g,D,a,false,40);
+% Calculate eta with the same time vector than the forces 
+eta=a*cos(omega.*t_Fcalc);
 
-figure()
-plot(t,eta,t,10^(-6)*F_tot,t,10^(-6)*M/2)
-xlabel('time','Fontsize',14)
+% Plot the time series with the overturning moment
+
+fQ5_1=figure();
+plot(t_Fcalc,eta,t_Fcalc,10^(-6)*F_tot,t_Fcalc,10^(-6)*M/2)
+xlabel('time (s)','Fontsize',14)
 ylabel('','Fontsize',14)
-legend({'Free surface elevation','Inline force','Overturning moment'},'Fontsize',12)
+legend({'Free surface elevation[m]','Inline force[MN]','Overturning moment [MN.m]'},'Fontsize',12)
 title('Time series','Fontsize',14)
 enhance_plot('TIMES',16,1.5)
+saveas(fQ5_1,'Q5_1.png');
 
-%what about it ? compare phases ?  
+NUM = [10 15 20 25 30 35 40 50 70 90]; % trying different number of points in the vertical direction
 
-NUM = [10 15 20 25 30 35 40 50 70 90]; %trying different number of points in the horizontal direction
+% We will compare the amplitude of the inline force and overturning moment
+% to see the effect of the number of points
 
+F_amplitudes = zeros(1,length(NUM));
+M_amplitudes = zeros(1,length(NUM));
 
 for i =1:(length(NUM))
-    [~,~,F_tot5(i,:),M5(i,:)] = FCalc(f,h,g,D,a,false,NUM(i));
+    [~,~,F5,M5] = FCalc(f,h,g,D,a,false,NUM(i));
+    F_amplitudes(i)=max(F5);
+    M_amplitudes(i)=max(M5);
 end
 
-figure()
+% Plot
+
+fQ5_2=figure();
 
 subplot(1,2,1)
 hold on
-plot(NUM, 10^(-6)*max(F_tot5'))
+plot(NUM, 10^(-6)*F_amplitudes)
 xlabel('Points along the vertical')
 ylabel('F [MN]')
 title('Inline force')
 enhance_plot('TIMES',16,1.5)
-plot(NUM,10^(-6)*max(F_tot5(end,:))*ones(1,length(NUM)),'k')
+plot(NUM,10^(-6)*F_amplitudes(end)*ones(1,length(NUM)),'k')
 hold off
 
 subplot(1,2,2)
-plot(NUM, 10^(-6)*max(M5'))
+plot(NUM, 10^(-6)*M_amplitudes)
 xlabel('Points along the vertical')
-ylabel('M [MN m]') %check unit
+ylabel('M [MN.m]')
 title('Overturning moment')
 enhance_plot('TIMES',16,1.5)
 hold on
-plot(NUM,10^(-6)*max(M5(end,:))*ones(1,length(NUM)),'k')
+plot(NUM,10^(-6)*M_amplitudes(end)*ones(1,length(NUM)),'k')
+
+saveas(fQ5_2,'Q5_2.png');
 
 
